bd08fa58a35d919fc97bfa27c28ccf2064647b95
Tom Evanko <tevanko@outlook.com>
2025-03-23 10:02:56 -0400

    Testing a push after account name change

--- tomsportfolio/src/pages/CodingNightmares.jsx
+++ tomsportfolio/src/pages/CodingNightmares.jsx
@@ -3,6 +3,7 @@ export default function CodingNightmares() {
     return (
         <Layout>
             <h1 className="">Tom's Coding Nightmares</h1>
+            <h3>Where to even begin?</h3>
         </Layout>
     )
 }
